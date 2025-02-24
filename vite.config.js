28e373cd05d5d8a0abce6831afd67159fe0f1198
Mei Huang <meihen0922@gmail.com>
2025-02-24 10:01:35 +0800

    build: add tailwind

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
 import { defineConfig } from "vite";
 import viteReact from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [viteReact()],
+  plugins: [viteReact(), tailwindcss()],
   test: {
     globals: true,
     environment: "jsdom",
