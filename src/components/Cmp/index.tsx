06a463b1a89d43d20796cba572ffc381fccb1d75
Mei Huang <meihen0922@gmail.com>
2025-02-26 17:46:03 +0800

    feat: 動態組件新增改變大小功能

--- src/components/Cmp/index.tsx
+++ src/components/Cmp/index.tsx
@@ -6,6 +6,8 @@ import { CanvasContext } from "../../Context";
 const editStyle = "box-content absolute";
 const unselectedStyle = "border border-transparent";
 const selectedStyle = "border border-[#02dcf7]";
+const stretchDotStyle =
+  "z-[999] absolute w-2 h-2 bg-[#00b3ff] rounded-full border border-transparent box-content hover:border-[#00b3ff] hover:bg-white";
 
 // TODO: 刪除，改變層級~
 // * 因為未來會太複雜，會需要很多 useCallback, useMemo，改用類組件
@@ -27,12 +29,6 @@ export default class Cmp extends Component<{
     this.setSelected();
     const startX = e.pageX;
     const startY = e.pageY;
-    console.log(
-      "%csrc/components/Cmp/index.tsx:18 onDragStart",
-      "color: #26bfa5;",
-      startX,
-      startY
-    );
     /**
      * * DataTransfer物件用於拖曳並放置（拖放）進程資料。
      * * https://developer.mozilla.org/zh-CN/docs/Web/API/DataTransfer
@@ -40,25 +36,117 @@ export default class Cmp extends Component<{
     e.dataTransfer.setData("text", `${startX},${startY}`);
   };
 
+  onMouseDown = (e: React.MouseEvent<HTMLUListElement, MouseEvent>) => {
+    const dir = e.target instanceof HTMLElement && e.target?.dataset.direction;
+    if (!dir) return;
+    e.stopPropagation();
+    e.preventDefault();
+    // 拖動前位置
+    let startX = e.pageX;
+    let startY = e.pageY;
+
+    // 暫時的結束位置
+    const move = (e: MouseEvent) => {
+      const { cmp } = this.props;
+      const { style } = cmp;
+      const x = e.pageX;
+      const y = e.pageY;
+      let disX = x - startX;
+      let disY = y - startY;
+      let newStyle: Record<string, any> = {};
+
+      // 只有左邊發生變化才會動到 left，需要減去移動距離才是真正的位置
+      if (dir.indexOf("left") >= 0) {
+        disX = 0 - disX;
+        newStyle.left = style.left - disX;
+      }
+      // 只有上方發生變化才會動到 top，需要減去移動距離才是真正的位置
+      if (dir.indexOf("top") >= 0) {
+        disY = 0 - disY;
+        newStyle.top = style.top - disY;
+      }
+
+      Object.assign(newStyle, {
+        width: style.width + disX,
+        height: style.height + disY,
+      });
+
+      this.context.updateSelectedCmp(newStyle);
+      startX = x;
+      startY = y;
+    };
+
+    const up = () => {
+      document.removeEventListener("mousemove", move);
+      document.removeEventListener("mouseup", up);
+    };
+
+    document.addEventListener("mousemove", move);
+    document.addEventListener("mouseup", up);
+  };
+
   render(): ReactNode {
     const { style, value } = this.props.cmp;
+    const { width, height } = style;
     return (
       <div onDragStart={this.onDragStart} draggable onClick={this.setSelected}>
         {/* 組件本身 */}
         <div style={style}>{value}</div>
         {/* 組件功能和選中的樣式 */}
-        <div
+        <ul
           className={classNames(
             editStyle,
             this.props.isSelected ? selectedStyle : unselectedStyle
           )}
           style={{
-            top: style.top - 2,
-            left: style.left - 2,
+            top: style.top - 1,
+            left: style.left - 1,
             width: style.width,
             height: style.height,
           }}
-        ></div>
+          onMouseDown={this.onMouseDown}
+        >
+          <li
+            className={stretchDotStyle}
+            style={{ top: -4, left: -4 }}
+            data-direction="top left"
+          />
+          <li
+            className={stretchDotStyle}
+            style={{ top: -4, left: width / 2 - 4 }}
+            data-direction="top"
+          />
+          <li
+            className={stretchDotStyle}
+            style={{ top: -4, left: width - 4 }}
+            data-direction="top right"
+          />
+          <li
+            className={stretchDotStyle}
+            style={{ top: height / 2 - 4, left: width - 4 }}
+            data-direction="right"
+          />
+          <li
+            className={stretchDotStyle}
+            style={{ top: height - 4, left: width - 4 }}
+            data-direction="bottom right"
+          />
+          <li
+            className={stretchDotStyle}
+            style={{ top: height - 4, left: width / 2 - 4 }}
+            data-direction="bottom"
+          />
+          <li
+            className={stretchDotStyle}
+            style={{ top: height - 4, left: -4 }}
+            data-direction="bottom left"
+          />
+          <li
+            className={stretchDotStyle}
+            style={{ top: height / 2 - 4, left: -4 }}
+            data-direction="left"
+          />
+        </ul>
       </div>
     );
   }
