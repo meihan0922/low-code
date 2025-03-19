c49570c52ef2f8f504ca725f1c9bb78e414a5b24
Mei Huang <meihen0922@gmail.com>
2025-03-19 15:49:59 +0800

    build(tsconfig): 新增alias path

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import viteReact from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,5 +9,13 @@ export default defineConfig({
   test: {
     globals: true,
     environment: "jsdom",
+  },
+  resolve: {
+    alias: [
+      {
+        find: "@",
+        replacement: path.resolve(__dirname, "src/"),
+      },
+    ],
   },
 });
